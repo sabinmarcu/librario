12b261879021a99bf03f6436b325fffa926b434d
Sabin Marcu <sabinmarcu@gmail.com>
2022-12-05 02:39:14 +0200

    fix(@librario/docs): fix the homepage message

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -10,14 +10,24 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />"
     >
       <div style={{
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '500px',
+        height: '500px',
+        maxWidth: '100vw',
+        maxHeight: '100vh',
         display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '100%',
+        textAlign: 'center',
+        flexDirection: 'column',
       }}
       >
-        <h1>Check out the Journey, above, while I fix this CSS</h1>
+        <h1>This is a log of the development process</h1>
+        <h2>What for? Check the links on the top right!</h2>
+        <h2>Click the top left button for the story itself</h2>
       </div>
     </Layout>
   );
